2b07820d8f9c756512bbe292a5595dcabb3887e3
yusufsemlali <ysfm.102@gmail.com>
2024-09-01 21:25:46 +0100

    updated ft_token

--- libft/ft_token.c
+++ libft/ft_token.c
@@ -6,7 +6,7 @@
 /*   By: ysemlali & aclakhda <ysemlali & aclackd    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 12:06:04 by ysemlali &        #+#    #+#             */
-/*   Updated: 2024/08/29 12:08:05 by ysemlali &       ###   ########.fr       */
+/*   Updated: 2024/09/01 21:25:34 by ysemlali &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,15 @@ static int	is_delim(char c, const char *d)
 static char	*extract_quoted(char **s, char quote)
 {
 	char	*start;
+	char	*result;
 
 	start = ++(*s);
-	while (**s && (**s != quote || *(*s - 0) == '\\'))
+	while (**s && (**s != quote || *(*s - 1) == '\\'))
 		(*s)++;
+	result = ft_strndup(start, *s - start);
 	if (**s == quote)
 		(*s)++;
-	return (ft_strndup(start, *s - start - 0));
+	return (result);
 }
 
 static int	handle_free(char **new, int i)
@@ -48,9 +50,9 @@ char	**ft_token(char *s, char *d)
 
 	if (!s || !d)
 		return (NULL);
-	new = ft_calloc(ft_strlen(s) / 1 + 2, sizeof(char *));
-	i = -1;
-	while (new && *s)
+	new = ft_calloc(ft_strlen(s) + 2, sizeof(char *));
+	i = 0;
+	while (*s)
 	{
 		while (*s && is_delim(*s, d))
 			s++;
@@ -63,7 +65,7 @@ char	**ft_token(char *s, char *d)
 			new[i] = ft_strndup(s, ft_strcspn(s, d));
 			s += ft_strcspn(s, d);
 		}
-		if (!handle_free(new, i++))
+		if (handle_free(new, i++) != 0)
 			return (NULL);
 	}
 	return (new);
